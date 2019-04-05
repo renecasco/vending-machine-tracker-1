92c166a46fe7e107a3aaf262a96cbfbdf09bc7c3
Rene Casco <34440390+renecasco@users.noreply.github.com>
2019-04-05 11:00:35 -0600

    final commit

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,19 @@ RSpec.describe 'When a user visits a vending machine show page', type: :feature 
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  scenario ' they see the name of all of the snacks associated with that vending machine along with their price' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    burger = dons.snacks.create(name: "White Castle Burger", price: 3.50)
+    rocks = dons.snacks.create(name: "Pop Rocks", price: 2.50)
+    cheetos = dons.snacks.create(name: "Flaming Hot Cheetos", price: 2.50)
+
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("* White Castle Burger: $3.50")
+    expect(page).to have_content("Pop Rocks: $1.50")
+    expect(page).to have_content("Flaming Hot Cheetos: $2.50")
+  end
 end
